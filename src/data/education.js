cb3b3c24e30d3ba45c7c2dd8676f690d83ff084f
Enrique Zetina <jenzetin@gmail.com>
2025-08-27 08:49:55 -0600

    Add new stlye for page

---
+++ src/data/education.js
@@ -0,0 +1,12 @@
+export const education = [
+  {
+    degree: "Master's Degree in Big Data",
+    institution: "Universidad Internacional de La Rioja (UNIR)",
+    period: "2019 - 2020"
+  },
+  {
+    degree: "Bachelor of Engineering in Computer Science",
+    institution: "Universidad Autónoma Metropolitana (UAM)",
+    period: "2010 - 2015"
+  }
+];
---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: 'Weather Dashboard',
+    category: 'Cloud',
+    image: '/img//weather.png',
+    tags: ['React', 'Node.js', 'AWS', 'API'],
+    description: 'Developed a cloud-based weather dashboard that fetches real-time weather data from an external API and displays it in an intuitive UI.',
+    liveUrl: null,
+    repoUrl: 'https://github.com/ezetina86/weather_app'
+  },
+  {
+    title: 'Automated News Aggregator Bot',
+    category: 'Python',
+    image: '/img/news-robot.png',
+    tags: ['Python', 'BeautifulSoup', 'MongoDB', 'Automation'],
+    description: 'Created a Python bot to scrape, process, and store news articles from various sources, providing a centralized and categorized news feed.',
+    liveUrl: null,
+    repoUrl: 'https://github.com/ezetina',
+  },
+  ,
+  {
+    title: 'Exam Generator',
+    category: 'Python',
+    image: '/img/exam_generator.png',
+    tags: ['Python', 'Flask', 'MongoDB', 'CI/CD', 'GCP', 'Generative AI', 'Docker'],
+    description: 'Certification exam generator that scrapes questions from various sources, processes them, and generates practice exams for users.',
+    liveUrl: null,
+    repoUrl: 'https://github.com/ezetina86/exam-generator',
+  }
+];
+
+export const categories = ['All', 'Cloud', 'DevOps', 'Big Data', 'Python'];
